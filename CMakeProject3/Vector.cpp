0ee3876c782cfbdc411b27c8d94c81e5d84c5712
celina1220 <wangweixidian@gmail.com>
2021-07-03 22:08:05 -0700

    Add project files.

---
+++ CMakeProject3/Vector.cpp
@@ -0,0 +1,84 @@
+#include "Vector.h"
+#include <stdexcept>
+
+using namespace std;
+template<typename T>
+Vector<T>::Vector(int s)
+{
+	if (s < 0)
+		throw std::length_error{ "Vector constructor:negative size" };
+	elem = new T[s];
+	sz = s;
+}
+
+template<typename T>
+Vector<T>::Vector(std::initializer_list<T> lst) : elem{new T[lst.size()]}, sz{static_cast<int>(lst.size())}
+{
+	copy(lst.  begin(), lst.end(), elem);
+}
+
+template<typename T>
+T& Vector<T>::operator[](int i)
+{
+	if (i < 0 || size() <= i)
+		throw std::out_of_range{ "Vector::operator[]" };
+	
+	return elem[i];
+}
+
+template<typename T>
+int Vector<T>::size()
+{
+	return sz;
+}
+
+template<typename T>
+Vector<T>::~Vector()
+{
+	delete[] elem;
+}
+
+//copy construct
+template<typename T>
+Vector<T>::Vector(const Vector<T>& a) : elem{new T[a.sz]}, sz{a.sz}
+{
+	 for (int i = 0; i != sz; i++)
+	 {
+		 elem[i] = a.elem[i];
+	 }
+}
+
+//copy assignment
+template<typename T>
+Vector<T>& Vector<T>::operator=(const Vector<T>& a)
+{
+	T* p = new double[a.sz];
+	for (int i = 0; i != a.sz; i++)
+	{
+		p[i] = a.elem[i];
+	}
+	delete[] elem;
+	elem = p;
+	sz = a.sz;
+	return *this;
+}
+
+//move assignment
+template<typename T>
+Vector<T>::Vector(Vector&& a):elem{a.elem}, sz{a.sz}
+{
+	a.elem = nullptr;
+	a.sz = 0;
+}
+
+template<typename T>
+void Vector<T>::push_back(T t)
+{
+	
+}
+
+
+
+
+
+
---
+++ CMakeProject3/Vector.h
@@ -0,0 +1,19 @@
+#include <initializer_list>
+
+template<typename T>
+class Vector {
+public:
+	Vector(std::initializer_list<T>);
+	explicit Vector(int s);
+	T& operator[](int i);
+	int size();
+	~Vector();
+	Vector(const Vector& a);
+	Vector& operator=(const Vector& a);
+	Vector(Vector&& a);
+	void push_back(T t);
+private:
+	T* elem;
+	int sz;
+};
+
