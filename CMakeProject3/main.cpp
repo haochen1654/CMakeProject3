294209892bd19041a3a186fe362c68cf5ae2ee09
haochen1654 <hao.chen1654@gmail.com>
2021-07-04 23:36:06 -0700

    Added endl to Cout

--- CMakeProject3/main.cpp
+++ CMakeProject3/main.cpp
@@ -11,11 +11,11 @@ int main() {
 	Vector<int> v = Vector<int>(5);
 	for (int i = 0; i < 10; i++) {
 		v.push_back(i);
-		cout << " size is :" << v.size() << "capacity is :" << v.capacity();
+		cout << " size is : " << v.size() << " capacity is : " << v.capacity() << endl;
 	}
 
 	for (int i = 0; i < v.size(); i ++) {
-		cout << v[i];
+		cout << v[i] << endl;
 	}
 	return 0;
 }
